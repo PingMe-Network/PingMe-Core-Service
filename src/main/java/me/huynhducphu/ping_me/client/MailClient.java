f066f627886e01b0d92caef529ef127a86510bbb
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-10 23:12:04 +0700

    refactor(core): Configure MailClient with internal auth
    This commit refactors the MailClient Feign configuration to use internal authentication.
    
    Changes include:
    - MailClient is now explicitly configured with MailFeignConfig for internal authentication.
    - MailFeignConfig now includes the logic for creating an `internalAuthInterceptor` to sign requests with a HMAC-SHA256 signature using the application's internal secret, timestamp, and request path.
    - Added new internal secret and service-name properties to application.properties.

--- src/main/java/me/huynhducphu/ping_me/client/MailClient.java
+++ src/main/java/me/huynhducphu/ping_me/client/MailClient.java
@@ -1,5 +1,6 @@
 package me.huynhducphu.ping_me.client;
 
+import me.huynhducphu.ping_me.config.feign.MailFeignConfig;
 import me.huynhducphu.ping_me.dto.base.ApiResponse;
 import me.huynhducphu.ping_me.dto.request.mail.SendOtpRequest;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -11,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @mailto : phatdang19052004@gmail.com
  * @created : 18/01/2026, Sunday
  **/
-@FeignClient(name = "mail-service", url = "${app.mail-service.url}")
+@FeignClient(
+        name = "mail-service",
+        url = "${app.mail-service.url}",
+        configuration = MailFeignConfig.class
+)
 public interface MailClient {
 
     @PostMapping("/mail-management/api/v1/mails/admin-otp-verification")
--- src/main/java/me/huynhducphu/ping_me/config/feign/MailFeignConfig.java
+++ src/main/java/me/huynhducphu/ping_me/config/feign/MailFeignConfig.java
@@ -1,6 +1,10 @@
 package me.huynhducphu.ping_me.config.feign;
 
 import feign.RequestInterceptor;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import me.huynhducphu.ping_me.utils.crypt.HmacUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,37 +17,27 @@ import java.util.Base64;
  * Admin 2/10/2026
  *
  **/
-//@Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class MailFeignConfig {
 
-//    @Bean
-//    public RequestInterceptor internalAuthInterceptor(
-//            @Value("${app.internal.secret}") String secret
-//    ) {
-//        return request -> {
-//            long ts = System.currentTimeMillis() / 1000;
-//
-//            String payload = request.method()
-//                    + request.url()
-//                    + ts;
-//
-//            String signature = hmacSha256(secret, payload);
-//
-//            request.header("X-Internal-Service", "core");
-//            request.header("X-Timestamp", String.valueOf(ts));
-//            request.header("X-Internal-Signature", signature);
-//        };
-//    }
-//
-//    private String hmacSha256(String secret, String data) {
-//        try {
-//            Mac mac = Mac.getInstance("HmacSHA256");
-//            mac.init(new SecretKeySpec(secret.getBytes(), "HmacSHA256"));
-//            return Base64
-//                    .getEncoder()
-//                    .encodeToString(mac.doFinal(data.getBytes()));
-//        } catch (Exception e) {
-//            throw new IllegalStateException("Cannot sign request", e);
-//        }
-//    }
+    @Value("${app.internal.secret}")
+    String secret;
+
+    @Bean
+    public RequestInterceptor internalAuthInterceptor() {
+        return request -> {
+            long ts = System.currentTimeMillis() / 1000;
+
+
+            String fullUrl = request.url();
+            String path = fullUrl.substring(fullUrl.indexOf("/", 8));
+
+            String payload = request.method() + path + ts;
+            String signature = HmacUtils.hmacSha256(secret, payload);
+
+            request.header("X-Timestamp", String.valueOf(ts));
+            request.header("X-Internal-Signature", signature);
+        };
+    }
+
 }
