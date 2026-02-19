099475ecc03cb1f638928da4ffb9e27a7fb8ea77
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-20 01:31:13 +0700

    refactor(AIChatRoom): Refactor AIChatRoom for better Lombok usage

--- src/main/java/me/huynhducphu/ping_me/model/ai/AIChatRoom.java
+++ src/main/java/me/huynhducphu/ping_me/model/ai/AIChatRoom.java
@@ -1,10 +1,8 @@
 package me.huynhducphu.ping_me.model.ai;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -28,37 +26,50 @@ import java.util.UUID;
 @CompoundIndex(name = "idx_user_updated", def = "{'user_id': 1, 'updated_at': -1}")
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class AIChatRoom implements Persistable<UUID> {
-    @Id
-    @EqualsAndHashCode.Include
-    private UUID id;
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AIChatRoom implements Persistable<@NonNull UUID> {
 
-    @Indexed
-    @Field("user_id")
-    private Long userId;
+    @Id
+    UUID id;
+
+    /**
+     * =====================================
+     * Nội dung chính
+     * =====================================
+     */
 
     @Indexed
     @Field("title")
-    private String title;
+    String title;
 
     @Field("total_msg_count")
-    private int totalMsgCount = 0;
+    int totalMsgCount = 0;
 
     @Field("latest_summary")
-    private String latestSummary; // Ký ức dài hạn tích lũy từ GPT-5 Nano
+    String latestSummary; // Ký ức dài hạn tích lũy từ GPT-5 Nano
 
     @Field("msg_count_since_last_summary")
-    private int interactCountSinceLastSummary = 0; // Để biết khi nào đạt ngưỡng 10-20 tin để tóm tắt tiếp
+    int interactCountSinceLastSummary = 0; // Để biết khi nào đạt ngưỡng 10-20 tin để tóm tắt tiếp
 
     @CreatedDate
     @Field("created_at")
-    private LocalDateTime createdAt;
+    LocalDateTime createdAt;
 
-    @LastModifiedDate // Dùng đúng annotation cho ngày cập nhật
+    @LastModifiedDate
     @Field("updated_at")
-    private LocalDateTime updatedAt;
+    LocalDateTime updatedAt;
+
+    /**
+     * =====================================
+     * Quan hệ phụ thuộc
+     * =====================================
+     */
+
+    @Indexed
+    @Field("user_id")
+    Long userId;
 
     @JsonIgnore
     @Override
